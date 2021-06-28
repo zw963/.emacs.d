2cf4aa2bcece5353132941049118138baa536070
Billy.Zheng <vil963@gmail.com>
2021-06-28 22:12:52 +0800

    Try enable company in telega

--- plugins/telega/telega_init.el
+++ plugins/telega/telega_init.el
@@ -3,6 +3,15 @@
 (setq telega-chat-show-avatars t)
 (setq telega-use-images t)
 
+(defun my-telega-chat-mode ()
+  (set (make-local-variable 'company-backends)
+       (append (list telega-emoji-company-backend
+                     'telega-company-username
+                     'telega-company-hashtag)))
+  (company-mode 1)
+  )
+(add-hook 'telega-chat-mode-hook 'my-telega-chat-mode)
+
 (provide 'telega_init)
 
 ;;; telega_init.el ends here
