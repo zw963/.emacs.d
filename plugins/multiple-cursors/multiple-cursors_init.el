09f58566b5ebc78ad71b33d4c474692454b13a3e
Billy.Zheng <vil963@gmail.com>
2021-07-25 15:18:25 +0800

    Add .mc-lists.el

--- plugins/multiple-cursors/multiple-cursors_init.el
+++ plugins/multiple-cursors/multiple-cursors_init.el
@@ -3,12 +3,14 @@
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 
 ;; 用法, 先给你要选择的 symbol 设置 mark, 然后按下 C->
-(    global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "M-j") 'mc/mark-next-like-this)
-(    global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 ;;(global-set-key (kbd "C-u C-@") 'set-rectangular-region-anchor)
 ;; (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
+
+(setq mc/list-file (expand-file-name "plugins/multiple-cursors/.mc-lists.el"))
 
 (add-hook 'activate-mark-hook '(lambda ()
                                  (local-set-key (kbd "C-@") 'set-rectangular-region-anchor)
