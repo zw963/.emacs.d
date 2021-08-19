db645a43ff06c0c12cced3e1f65a06c6e8a29d94
Billy.Zheng <vil963@gmail.com>
2021-08-19 15:15:52 +0800

    Update tabnine config.

--- plugins/company-mode/plugins/company-tabnine/company-tabnine_init.el
+++ plugins/company-mode/plugins/company-tabnine/company-tabnine_init.el
@@ -1,12 +1,16 @@
 (require 'company-tabnine)
 
+;; (setq company-tabnine-binaries-folder (expand-file-name "plugins/company-mode/plugins/company-tabnine/"))
+
 (defun add-tabnine-backend ()
   (make-local-variable 'company-backends)
+  (setq-local company-idle-delay 0)
   (add-to-list 'company-backends #'company-tabnine)
   )
 
 (with-eval-after-load 'rust-mode (add-hook 'rust-mode-hook 'add-tabnine-backend))
 (with-eval-after-load 'sh-mode (add-hook 'sh-mode-hook 'add-tabnine-backend))
+(with-eval-after-load 'enh-ruby-mode (add-hook 'enh-ruby-mode-hook 'add-tabnine-backend))
 
 (provide 'company-tabnine_init)
 
