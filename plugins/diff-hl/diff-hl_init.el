def223e2fad989946eebf653cb19fd4aad23223a
Billy.Zheng <vil963@gmail.com>
2021-09-08 17:30:07 +0800

    Fix diff-hl hotkey

--- plugins/diff-hl/diff-hl_init.el
+++ plugins/diff-hl/diff-hl_init.el
@@ -13,12 +13,19 @@
 (require 'diff-hl-flydiff)
 (diff-hl-flydiff-mode 1)
 
-;; (defun show-first-hunk-on-current-buffer ()
-;;   (interactive)
-;;   (beginning-of-buffer)
-;;   (diff-hl-show-hunk-next)
-;;   )
-;; (define-key diff-hl-command-map [(n)] 'show-first-hunk-on-current-buffer)
+(defun diff-hl-show-hunk-or-show-first-hunk ()
+  (interactive)
+  (if (diff-hl-hunk-overlay-at (point))
+      (diff-hl-show-hunk)
+    (progn
+      (beginning-of-buffer)
+      (diff-hl-show-hunk-next))
+    ))
+
+(define-key diff-hl-command-map [(*)] 'diff-hl-show-hunk-or-show-first-hunk)
+(define-key diff-hl-show-hunk--inline-popup-map (kbd "p") #'diff-hl-show-hunk-previous)
+(define-key diff-hl-show-hunk--inline-popup-map (kbd "n") #'diff-hl-show-hunk-next)
+(define-key diff-hl-show-hunk--inline-popup-map (kbd "r") #'diff-hl-show-hunk-revert-hunk)
 
 ;; (require 'diff-hl-show-hunk-posframe)
 ;; (setq diff-hl-show-hunk-function 'diff-hl-show-hunk-posframe)
