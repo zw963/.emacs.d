80f35336cd145059190bad13bc6d498a92cad439
Billy.Zheng <vil963@gmail.com>
2021-09-08 23:30:58 +0800

    Add back-button

---
+++ plugins/back-button/back-button_init.el
@@ -0,0 +1,19 @@
+(require 'back-button)
+
+(back-button-mode 1)
+
+(with-eval-after-load 'hydra
+  (defhydra hydra-back-button
+    (
+     global-map "C-x ,"
+     :pre (setq cursor-type t)
+     :post (setq cursor-type 'bar)
+     )
+    "global mark ring"
+    ("n" back-button-global-forward "global next mark")
+    ("p" back-button-global-backward "global previous hunk")))
+
+
+(provide 'back-button_init)
+
+;;; back-button_init.el ends here
