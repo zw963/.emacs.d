9b8f636efa16ea80adcce5dc3193d13b311f5145
Billy.Zheng <vil963@gmail.com>
2021-09-09 23:17:03 +0800

    Disable pangu global mode

--- plugins/pangu-spacing/pangu-spacing_init.el
+++ plugins/pangu-spacing/pangu-spacing_init.el
@@ -1,14 +1,16 @@
 (require 'pangu-spacing)
 
-(global-pangu-spacing-mode 1)
-
 (add-hook 'org-mode-hook
           '(lambda ()
-             (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)))
+             (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)
+             (pangu-spacing-mode 1)
+             ))
 
 (add-hook 'markdown-mode-hook
           '(lambda ()
-             (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)))
+             (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)
+             (pangu-spacing-mode 1)
+             ))
 
 (provide 'pangu-spacing_init)
 
