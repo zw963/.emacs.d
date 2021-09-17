6a2891fd3ca8fd16e3bc98e0047b37ae1e5e85c4
Billy.Zheng <vil963@gmail.com>
2021-09-17 19:35:40 +0800

    remove mwim, add helpful

---
+++ plugins/helpful/helpful_init.el
@@ -0,0 +1,13 @@
+(require 'helpful)
+
+(global-set-key [remap describe-function] 'helpful-callable)
+(global-set-key [remap Info-goto-emacs-command-node] 'helpful-function) ;; C-h F
+(global-set-key [remap describe-variable] 'helpful-variable)
+(global-set-key [remap describe-key] 'helpful-key)
+(global-set-key [remap describe-coding-system] 'helpful-command) ;; Ctrl+h C
+
+(global-set-key (kbd "C-c C-d") #'helpful-at-point)
+
+(provide 'helpful_init)
+
+;;; helpful_init.el ends here
--- plugins/mwim/mwim_init.el
+++
@@ -1,8 +0,0 @@
-(require 'mwim)
-
-(global-set-key (kbd "C-a") 'mwim-beginning)
-(global-set-key (kbd "C-e") 'mwim-end)
-
-(provide 'mwim_init)
-
-;;; mwim_init.el ends here
