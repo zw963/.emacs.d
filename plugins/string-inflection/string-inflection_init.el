73dc29684fb176328ff8142953f21acacd67087b
Billy.Zheng <vil963@gmail.com>
2021-10-23 23:46:35 +0800

    Change inflection cycle use alt + u

--- plugins/string-inflection/string-inflection_init.el
+++ plugins/string-inflection/string-inflection_init.el
@@ -17,18 +17,18 @@
     ;; default
     (string-inflection-ruby-style-cycle))))
 
-(global-set-key [(shift meta u)] 'my-string-inflection-cycle-auto)
+(global-set-key [(meta u)] 'my-string-inflection-cycle-auto)
 
-;; (require 'sequential-command)
-;; (defun backward-capitalize-word ()
-;;   (interactive)
-;;   (subword-capitalize (- (1+ (seq-count*)))))
+(require 'sequential-command)
+(defun backward-capitalize-word ()
+  (interactive)
+  (subword-capitalize (- (1+ (seq-count*)))))
 
-;; (defun backward-upcase-word ()
-;;   (interactive)
-;;   (subword-upcase (- (1+ (seq-count*)))))
+(defun backward-upcase-word ()
+  (interactive)
+  (subword-upcase (- (1+ (seq-count*)))))
 
-;; (global-set-key [(meta u)] 'backward-upcase-word) ; 连续标记光标前面的单词大写 Alt-u
+(global-set-key [(shfit meta u)] 'backward-upcase-word) ; 连续标记光标前面的单词大写 Alt-u
 ;; (global-set-key [(meta U)] 'backward-capitalize-word) ;连续标记光标前面单词首字母大写 Alt-U
 
 (provide 'string-inflection_init)
