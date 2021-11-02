a074edaabf3ce186bbf73590cc341c5872ca6c37
Billy.Zheng <vil963@gmail.com>
2021-11-03 01:05:46 +0800

    Make ibuffer Ctrl+o can open buffer on another window.

--- plugins/ibuffer/ibuffer_init.el
+++ plugins/ibuffer/ibuffer_init.el
@@ -25,6 +25,8 @@
 ;; 用来清楚桌面上所有的 buffer.
 (define-key ibuffer-mode-map [(f2)] 'desktop-clear)
 
+(add-hook 'ibuffer-mode-hook (lambda () (setq-local display-buffer-base-action '(display-buffer-use-some-window))))
+
 
 (provide 'ibuffer_init)
 ;;; ibuffer_init.el ends here
