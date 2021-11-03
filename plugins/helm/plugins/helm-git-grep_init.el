993679e3c78aeb0af72d684f89e027e18c21ecc0
Billy.Zheng <vil963@gmail.com>
2021-11-04 01:39:11 +0800

    helm-git-grep support editing.

--- plugins/helm/plugins/helm-git-grep_init.el
+++ plugins/helm/plugins/helm-git-grep_init.el
@@ -1,3 +1,10 @@
+;; wgrep 用来支持编辑 grep 结果的。
+;; 用法：
+;; helm-git-grep 出来结果后，按下 TAB, F3, C-c C-p, 然后编辑，完成后 C-c C-c.
+
+;; (require 'wgrep)
+;; (require 'wgrep-helm)
+
 (require 'helm-git-grep)
 
 (setq helm-git-grep-pathspecs '("*" ":!:*.min.js*" ":!:*.less" ":!:*/vendor/assets*"))
@@ -18,7 +25,12 @@
       (call-interactively 'helm-git-grep-at-point)
     (call-interactively 'helm-do-ag)))
 
+;; (push '("Save results in grep buffer" . helm-git-grep-save-results)
+;;       helm-git-grep-actions)
+;; (delete-dups helm-git-grep-actions)
+
 (global-set-key (kbd "M-r") 'helm-git-grep-at-point-or-helm-do-ag)
+(define-key isearch-mode-map (kbd "M-r") 'helm-git-grep-from-isearch)
 (define-key helm-map (kbd "M-r") 'helm-git-grep-from-helm)
 
 (provide 'helm-git-grep_init)
