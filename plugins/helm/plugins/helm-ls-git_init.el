9822673e3ac0ff15e5e03e531ef18eb502ed02fc
Billy.Zheng <vil963@gmail.com>
2021-11-04 01:12:15 +0800

    helm-ls-git remove branch source

--- plugins/helm/plugins/helm-ls-git_init.el
+++ plugins/helm/plugins/helm-ls-git_init.el
@@ -3,6 +3,9 @@
 (setq helm-ls-git-fuzzy-match t)
 (global-set-key [(control x) (control d)] 'helm-browse-project)
 
+(setq helm-ls-git-default-sources
+              (delete 'helm-ls-git-branches-source helm-ls-git-default-sources))
+
 (provide 'helm-ls-git_init)
 
 ;;; helm-ls-git_init.el ends here
