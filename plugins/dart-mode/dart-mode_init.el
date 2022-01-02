e117c20e800fbf38302bee364c120d8b5d10ae8e
Billy.Zheng <vil963@gmail.com>
2022-01-02 13:16:41 +0800

    lsp-dart can support specify entry point.

--- plugins/dart-mode/dart-mode_init.el
+++ plugins/dart-mode/dart-mode_init.el
@@ -15,23 +15,62 @@
 
 (with-eval-after-load 'lsp-mode
   (require 'lsp-dart)
+  (require 'lsp-dart-dap)
   (require 'lsp-dart-commands)
   (require 'lsp-dart-devtools)
   (require 'lsp-dart-outline)
-  (require 'lsp-dart-flutter-daemon)
-  (lsp-dart-flutter-daemon-mode)
-  (require 'lsp-dart-test-support)
+  (require 'lsp-dart-protocol)
+  (require 'lsp-dart-utils)
+  (require 'lsp-dart-code-lens)
+  (require 'lsp-dart-closing-labels)
+
+  ;; (require 'lsp-dart-flutter-daemon)
+  ;; (lsp-dart-flutter-daemon-mode)
+  (require 'lsp-dart-flutter-colors)
+  (require 'lsp-dart-flutter-fringe-colors)
+  (require 'lsp-dart-flutter-widget-guide)
+
+  ;; lsp-dart-test-output.el
+  ;; lsp-dart-test-support.el
+  ;; lsp-dart-test-tree.el
+
+
+  (defun lsp-dart-get-project-entrypoint ()
+    "Return the dart or flutter project entrypoint."
+    (let* ((root (lsp-dart-get-project-root))
+           (lib-entry (expand-file-name "lib/main_local.dart" root))
+           (bin-entry (expand-file-name "bin/main.dart" root)))
+      (cond
+       ((file-exists-p lib-entry)
+        lib-entry)
+
+       ((file-exists-p bin-entry)
+        bin-entry))))
+
+  (dap-register-debug-template
+   "Flutter :: Custom run"
+   (list :flutterPlatform "x86_64"
+         :program "lib/main_local.dart"
+         :type "flutter"
+         ))
+
+  (dap-register-debug-template
+   "Flutter :: Custom debug"
+   (list :flutterPlatform "x86_64"
+         :program "lib/main_local.dart"
+         :type "flutter"
+         :flutterMode "debug"))
+
   (setq lsp-signature-auto-activate nil)
   (setq lsp-dart-dap-flutter-hot-reload-on-save t)
   (add-hook 'dart-mode-hook 'lsp)
   )
 
 (require 'flutter)
-
 (setq flutter-l10n-arb-dir "lib/i10n")
 (setq flutter-l10n-template-arb-file "intl_zh_Hans.arb")
 
-(add-hook 'dart-mode-hook 'flutter-test-mode)
+;; (add-hook 'dart-mode-hook 'flutter-test-mode)
 (define-key dart-mode-map (kbd "C-M-x") 'flutter-run-or-hot-reload)
 
 (require 'hover)
