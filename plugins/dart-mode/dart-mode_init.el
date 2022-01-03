611c603d1295f550a0b8adf3dc73044727fa3ce2
Billy.Zheng <vil963@gmail.com>
2022-01-04 03:34:03 +0800

    Remove not used code in dart-mode_init.el

--- plugins/dart-mode/dart-mode_init.el
+++ plugins/dart-mode/dart-mode_init.el
@@ -14,28 +14,28 @@
 (add-to-list 'auto-mode-alist '("\\.dart\\'" . dart-mode))
 
 (with-eval-after-load 'lsp-mode
-  ;; (setq lsp-signature-auto-activate nil)
+  (setq lsp-signature-auto-activate nil)
   (setq lsp-dart-dap-flutter-hot-reload-on-save t)
   (add-hook 'dart-mode-hook 'lsp)
   )
 
-(with-eval-after-load 'lsp-treemacs
-  (add-hook 'dart-mode-hook 'treemacs-select-window)
-  )
+;; (with-eval-after-load 'lsp-treemacs
+;;   (add-hook 'dart-mode-hook 'treemacs-select-window)
+;;   )
 
-;; (require 'flutter)
-;; (setq flutter-l10n-arb-dir "lib/i10n")
-;; (setq flutter-l10n-template-arb-file "intl_zh_Hans.arb")
+(require 'flutter)
+(setq flutter-l10n-arb-dir "lib/i10n")
+(setq flutter-l10n-template-arb-file "intl_zh_Hans.arb")
 
-;; ;; (add-hook 'dart-mode-hook 'flutter-test-mode)
-;; (define-key dart-mode-map (kbd "C-M-x") 'flutter-run-or-hot-reload)
+;; (add-hook 'dart-mode-hook 'flutter-test-mode)
+(define-key dart-mode-map (kbd "C-M-x") 'flutter-run-or-hot-reload)
 
-;; (require 'hover)
+(require 'hover)
 
-;; (setq hover-command-path "hover")
-;; (setq hover-hot-reload-on-save t)
-;; (setq hover-clear-buffer-on-hot-restart t)
-;; (setq hover-screenshot-path "$HOME/Pictures")
+(setq hover-command-path "hover")
+(setq hover-hot-reload-on-save t)
+(setq hover-clear-buffer-on-hot-restart t)
+(setq hover-screenshot-path "$HOME/Pictures")
 
 (provide 'dart-mode_init)
 
