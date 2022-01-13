950c542287142b37495895b13747259bfd0098a1
Billy.Zheng <vil963@gmail.com>
2022-01-13 23:56:45 +0800

    Add popup config. (not use it)

---
+++ plugins/popup/popup_init.el
@@ -0,0 +1,15 @@
+(require 'popup)
+(load "popup_doom.el")
+(load "settings.el")
+
+(set-popup-rule! "\\*LSP Dart tests\\*" :height 0.3)
+(set-popup-rule! "\\*Hover\\*" :quit nil)
+(set-popup-rule! "\\*dap-ui-locals\\*" :side 'right :width 0.3)
+(set-popup-rule! "\\*dap-ui-sessions\\*" :side 'right :width 0.3)
+(set-popup-rule! "\\*midje-test-report\\*" :side 'right :width 0.5)
+(set-popup-rule! "*cider-test-report*" :side 'right :width 0.4)
+(set-popup-rule! "^\\*cider-repl" :side 'bottom :quit nil)
+
+(provide 'popup_init)
+
+;;; popup_init.el ends here
