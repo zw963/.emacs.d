4c6e8c5bd51a7a7833a0e0e7d024d0de59615090
Billy.Zheng <vil963@gmail.com>
2022-01-16 23:04:40 +0800

    Make popper pop from top

--- popper/popper_init.el
+++ popper/popper_init.el
@@ -1,19 +1,31 @@
 (require 'popper)
 (require 'popper-echo)
 
+;; 注意，有一些模式使用 mode-name 方式是无法生效的.
 (setq popper-reference-buffers
-      '("\\*Messages\\*"
+      '("\\*Messages\\*$"
         "Output\\*$"
+        ;; "LSP Error List\\*$"
         "\\*Async Shell Command\\*"
+        "\\*quickrun\\*$"
         "Scratch.txt"
         vterm-mode
         inf-ruby-mode
-        quickrun--mode
         (special-mode . hide)
         (hover-mode . hide)
         (compilation-mode . hide)
         help-mode
         ))
+
+(defun popper-select-popup-at-top (buffer &optional _alist)
+  "Display popup-buffer BUFFER at the bottom of the screen."
+  (let ((win (display-buffer-in-side-window
+              buffer
+              `((window-height . ,popper-window-height)
+                (side . top)
+                (slot . 1)))))
+    (select-window win)))
+(setq popper-display-function #'popper-select-popup-at-top)
 
 (global-set-key [(shift control t)] 'popper-toggle-latest)
 (with-eval-after-load 'vterm
