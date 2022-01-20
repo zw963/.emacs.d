a96f6c8d94ad98843f047737dab9e46e60a24037
Billy.Zheng <vil963@gmail.com>
2022-01-20 12:23:17 +0800

    popper_init.el not hide Hover-mode

--- popper/popper_init.el
+++ popper/popper_init.el
@@ -5,31 +5,29 @@
 (setq popper-reference-buffers
       '("\\*Messages\\*$"
         "Output\\*$"
-        ;; "LSP Error List\\*$"
         "\\*Async Shell Command\\*"
         "\\*quickrun\\*$"
-        ;; "Scratch.txt"
-        ("\\*Hover\\*" . hide)
         vterm-mode
         inf-ruby-mode
         (special-mode . hide)
-        (hover-mode . hide)
-        (compilation-mode . hide)
-        help-mode
+        ;; (hover-mode . hide)
+        ;; (compilation-mode . hide)
+        ;; help-mode
         ))
 
-;; (defun popper-select-popup-at-top (buffer &optional _alist)
-;;   "Display popup-buffer BUFFER at the bottom of the screen."
-;;   (let ((win (display-buffer-in-side-window
-;;               buffer
-;;               `((window-height . ,popper-window-height)
-;;                 (side . top)
-;;                 (slot . 1)))))
-;;     (select-window win)))
-;; (setq popper-display-function #'popper-select-popup-at-top)
+(defun popper-select-popup-at-top (buffer &optional _alist)
+  "Display popup-buffer BUFFER at the bottom of the screen."
+  (let ((win (display-buffer-in-side-window
+              buffer
+              `((window-height . ,popper-window-height)
+                (side . top)
+                (slot . 1)))))
+    (select-window win)))
+(setq popper-display-function #'popper-select-popup-at-top)
 
 ;; use shackle control popup show window
 (setq popper-display-control nil)
+(setq popper-window-height 50)
 
 (global-set-key [(shift control t)] 'popper-toggle-latest)
 (with-eval-after-load 'vterm
@@ -39,7 +37,6 @@
 (setq popper-group-function #'popper-group-by-directory)
 (popper-mode +1)
 (popper-echo-mode +1)
-;; (setq popper-window-height 50)
 
 (provide 'popper_init)
 
