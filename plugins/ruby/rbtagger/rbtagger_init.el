724b4db7d56c286a8dc5903c48488a7cfc344275
Billy.Zheng <vil963@gmail.com>
2022-01-24 15:49:03 +0800

    Disable rbtagger mode, but enable generate tag.

--- plugins/ruby/rbtagger/rbtagger_init.el
+++ plugins/ruby/rbtagger/rbtagger_init.el
@@ -3,18 +3,18 @@
 (setq tags-add-tables nil)
 (setq tags-revert-without-query t)
 
-(advice-add 'rbtagger-mode :before
-            (lambda ()
-              (unless (and rvm--current-ruby rvm--current-gemset)
-                (rvm-activate-corresponding-ruby))
-              ))
+;; (advice-add 'rbtagger-mode :before
+;;             (lambda ()
+;;               (unless (and rvm--current-ruby rvm--current-gemset)
+;;                 (rvm-activate-corresponding-ruby))
+;;               ))
 
 (add-hook 'after-save-hook
           (lambda ()
             (if (member major-mode '(ruby-mode enh-rub-mode))
                 (call-interactively 'rbtagger-generate-tags))))
 
-(run-ruby-mode-hook '(rbtagger-mode))
+;; (run-ruby-mode-hook '(rbtagger-mode))
 
 ;; (defun turn-on-helm-etags-for-ruby ()
 ;;   (local-set-key "\M-." 'helm-etags-select)
