665e9cd6d30809b733c809ac9086ba5b424e9716
Billy.Zheng <vil963@gmail.com>
2022-02-01 02:20:42 +0800

    Update mu4e

--- plugins/mu4e/mu4e_init.el
+++ plugins/mu4e/mu4e_init.el
@@ -96,6 +96,7 @@
                          ;; "程序员中的牛逼运维， 运维中的牛逼程序员。"
                          )
  gnus-unbuttonized-mime-types nil
+ ;; message-cite-reply-position 'below ;; 当回复消息时，我们写的消息，在引用的文字下面。
  mu4e-sent-folder   "/gmail/[Gmail].Sent Mail"
  mu4e-drafts-folder "/gmail/[Gmail].Drafts"
  mu4e-trash-folder  "/gmail/[Gmail].Trash"
