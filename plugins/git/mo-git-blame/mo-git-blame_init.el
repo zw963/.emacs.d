e5e3393efad00fbca5805cd28cf8321b2d0b47ba
Billy.Zheng <vil963@gmail.com>
2022-02-05 15:29:56 +0800

    Enable mo-git-blame again.

--- plugins/git/mo-git-blame/mo-git-blame_init.el
+++ plugins/git/mo-git-blame/mo-git-blame_init.el
@@ -3,9 +3,9 @@
 
 (setq mo-git-blame-delete-other-windows t)
 (global-set-key [(control x) (v) (g)] 'mo-git-blame-current)
-;; (define-key mo-git-blame-mode-map [(n)] 'next-line)
-;; (define-key mo-git-blame-mode-map [(p)] 'previous-line)
 
+(define-key mo-git-blame-mode-map [(n)] 'next-line)
+(define-key mo-git-blame-mode-map [(p)] 'previous-line)
 (define-key mo-git-blame-mode-map [(meta g) (meta g)] 'mo-git-blame-goto-line)
 
 (provide 'mo-git-blame_init)
