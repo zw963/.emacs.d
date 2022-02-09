f020a68c45253574ce59614fe94378596a6ab3a6
Billy.Zheng <vil963@gmail.com>
2022-02-10 02:00:29 +0800

    refore iedit meta p/n keybinding

--- plugins/iedit/iedit_init.el
+++ plugins/iedit/iedit_init.el
@@ -4,6 +4,8 @@
 (require 'iedit-rect)
 ;; 开启 iedit-rectangle-mode 之后, 可以直接编辑, 编辑完记住要再次快捷键退出模式.
 (global-set-key [(control meta \7)] 'iedit-rectangle-mode)
+(define-key iedit-lib-keymap [(meta p)] 'iedit-prev-occurrence)
+(define-key iedit-lib-keymap [(meta n)] 'iedit-next-occurrence)
 
 (provide 'iedit_init)
 ;;; iedit_init.el ends here
