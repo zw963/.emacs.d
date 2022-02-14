c0d9249c0435be500c58b197c1fc0d65040b467a
Billy.Zheng <vil963@gmail.com>
2022-02-14 19:15:45 +0800

    Add highlight-indentation-face

--- plugins/highlight-indentation/highlight-indentation_init.el
+++ plugins/highlight-indentation/highlight-indentation_init.el
@@ -1,5 +1,8 @@
 (require 'highlight-indentation)
 
+;; (set-face-background 'highlight-indentation-face "#333333")
+;; (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+
 (dolist (hook '(coffee-mode-hook
                 feature-mode-hook
                 yaml-mode-hook
