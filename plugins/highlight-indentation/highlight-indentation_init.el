3796e85ce47e65072cd343bd6d482c537d88020f
Billy.Zheng <vil963@gmail.com>
2022-02-15 23:48:18 +0800

    Fix  highlight-indentation color

--- plugins/highlight-indentation/highlight-indentation_init.el
+++ plugins/highlight-indentation/highlight-indentation_init.el
@@ -1,12 +1,13 @@
 (require 'highlight-indentation)
 
-;; (set-face-background 'highlight-indentation-face "#333333")
-;; (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+(set-face-background 'highlight-indentation-face "#333333")
+(set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
 
 (dolist (hook '(coffee-mode-hook
                 feature-mode-hook
                 yaml-mode-hook
                 python-mode-hook
+                dart-mode-hook
                 slim-mode-hook))
   (add-hook hook 'highlight-indentation-mode))
 
