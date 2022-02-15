4153f606cf8f96bff0e2d9f40b6710e1f14485d3
Billy.Zheng <vil963@gmail.com>
2022-02-15 23:47:46 +0800

    Fix helm-ag color

--- plugins/helm/plugins/helm-ag_init.el
+++ plugins/helm/plugins/helm-ag_init.el
@@ -13,7 +13,7 @@
  '(helm-follow-mode-persistent t)
  ;; helm-ag-base-command "ag --nocolor --nogroup --ignore-case"
  ;; '(helm-ag-base-command "rg --hidden --with-filename --no-heading --smart-case")
- '(helm-ag-base-command "rg --no-config --no-heading --hidden --smart-case")
+ '(helm-ag-base-command "rg --no-config --no-heading --hidden --smart-case --color never")
  ;; '(helm-ag-base-command "rg --no-config --no-heading --hidden --smart-case" )
  '(helm-ag-insert-at-point 'symbol)
  '(helm-ag-fuzzy-match t)
