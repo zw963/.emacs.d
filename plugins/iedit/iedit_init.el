83fbe97bf9b698bc116508f211cc4dc6a83385ed
Billy.Zheng <vil963@gmail.com>
2022-04-01 23:28:29 +0800

    update iedit

--- plugins/iedit/iedit_init.el
+++ plugins/iedit/iedit_init.el
@@ -1,6 +1,9 @@
 (require 'iedit)
+
 (setq-default iedit-occurrence-context-lines 0)
 (setq iedit-auto-narrow t)
+(setq iedit-auto-buffering t)
+
 (require 'iedit-rect)
 ;; 开启 iedit-rectangle-mode 之后, 可以直接编辑, 编辑完记住要再次快捷键退出模式.
 (global-set-key [(control meta \7)] 'iedit-rectangle-mode)
