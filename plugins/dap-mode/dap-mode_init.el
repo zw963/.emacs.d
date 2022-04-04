1f91a8fbc4625e61fffdc9d22fc86ed189a27055
Billy.Zheng <vil963@gmail.com>
2022-04-05 02:36:01 +0800

    update dap-mode

--- plugins/dap-mode/dap-mode_init.el
+++ plugins/dap-mode/dap-mode_init.el
@@ -1,19 +1,13 @@
 (setq dap-utils-extension-path (expand-file-name ".extension" (file-name-directory (or load-file-name buffer-file-name))))
 
-(require 'dap-mode)
-(dap-mode 1)
-
-(require 'dap-ui)
-(dap-ui-mode 1)
-
 (require 'dap-mouse)
-(dap-tooltip-mode 1)
+;; (require 'dapui)
 
-;; (require 'dap-ruby)
-;; (dap-ruby-setup)
+;; (dap-mode 1)
+;; (dap-tooltip-mode 1)
+;; (dap-ui-mode 1)
 
-;; (require 'dap-go)
-;; (dap-go-setup)
+;; (require 'dap-hydra)
 
 ;; Try to connect to ruby-debug-ide, but still working.
 
