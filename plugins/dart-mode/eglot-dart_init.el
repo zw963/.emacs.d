18085cf3292df136f4d502a57af8fbcb988d1a52
Billy.Zheng <vil963@gmail.com>
2022-04-06 01:29:13 +0800

    remove hack for eglot dart server

--- plugins/dart-mode/eglot-dart_init.el
+++ plugins/dart-mode/eglot-dart_init.el
@@ -1,8 +1,5 @@
 (require 'eglot_init)
 
-(setq eglot-server-programs (remove '(dart-mode "dart_language_server") eglot-server-programs))
-(add-to-list 'eglot-server-programs '(dart-mode . ("dart" "language-server")))
-
 (add-hook 'dart-mode-hook 'eglot-ensure)
 
 (provide 'eglot-dart_init)
