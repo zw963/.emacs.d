52024395190bf8794b4fd90db3c8f4b0ccaabf68
Billy.Zheng <vil963@gmail.com>
2022-04-08 15:49:15 +0800

    Add color-rg ignore files

--- plugins/color-rg/color-rg_init.el
+++ plugins/color-rg/color-rg_init.el
@@ -1,5 +1,17 @@
 (require 'color-rg)
 
+(setq color-rg-search-ignore-rules "\
+-g \"!*~\" \
+-g \"!#*#\" \
+-g \"!*.min.*\" \
+-g \"!TAGS\" \
+-g \"!tags\" \
+-g \"!.git/\" \
+-g \"!node_modules\" \
+-g \"!dist\" \
+-g \"!*.log\" \
+")
+
 (provide 'color-rg_init)
 
 ;;; color-rg_init.el ends here
