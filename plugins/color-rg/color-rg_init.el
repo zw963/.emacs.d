9f0f47958b2490c8216e91048afc19e7d5df8a4c
Billy.Zheng <vil963@gmail.com>
2022-04-09 17:42:10 +0800

    update color-rg keybinding

--- plugins/color-rg/color-rg_init.el
+++ plugins/color-rg/color-rg_init.el
@@ -12,6 +12,13 @@
 -g \"!*.log\" \
 ")
 
+(add-hook 'color-rg-mode-hook
+          (lambda ()
+            (define-key color-rg-mode-map (kbd "n") 'color-rg-next-file)
+            (define-key color-rg-mode-map (kbd "p") 'color-rg-prev-file)
+           ))
+
+
 (provide 'color-rg_init)
 
 ;;; color-rg_init.el ends here
