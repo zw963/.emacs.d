cc3fbffe57810c9a4bc4f9932d32871e7a837c1d
Billy.Zheng <vil963@gmail.com>
2022-04-22 15:19:19 +0800

    helm-ls-git disable fuzzy

--- plugins/helm/plugins/helm-ls-git_init.el
+++ plugins/helm/plugins/helm-ls-git_init.el
@@ -1,10 +1,10 @@
 (require 'helm-ls-git)
 ;; 这个最近打开的.
-(setq helm-ls-git-fuzzy-match t)
+;; (setq helm-ls-git-fuzzy-match t)
 (global-set-key [(control x) (control d)] 'helm-browse-project)
 
-(setq helm-ls-git-default-sources
-              (delete 'helm-ls-git-branches-source helm-ls-git-default-sources))
+;; (setq helm-ls-git-default-sources
+;;               (delete 'helm-ls-git-branches-source helm-ls-git-default-sources))
 
 (provide 'helm-ls-git_init)
 
