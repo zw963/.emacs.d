f68095066beac7427f48267c292a5ef439ee8d22
Billy.Zheng <vil963@gmail.com>
2022-04-24 12:07:06 +0800

    Remove git branch/stash sources from helm-ls-git

--- plugins/helm/plugins/helm-ls-git_init.el
+++ plugins/helm/plugins/helm-ls-git_init.el
@@ -3,8 +3,11 @@
 ;; (setq helm-ls-git-fuzzy-match t)
 (global-set-key [(control x) (control d)] 'helm-browse-project)
 
-;; (setq helm-ls-git-default-sources
-;;               (delete 'helm-ls-git-branches-source helm-ls-git-default-sources))
+(setq helm-ls-git-default-sources
+      (delete 'helm-ls-git-stashes-source
+              (delete 'helm-ls-git-create-branch-source
+                      (delete 'helm-ls-git-branches-source
+                              helm-ls-git-default-sources))))
 
 (provide 'helm-ls-git_init)
 
