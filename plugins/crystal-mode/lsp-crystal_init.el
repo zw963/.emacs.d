5fc888109085633853f53aabd0cf6252876b1f07
Billy.Zheng <vil963@gmail.com>
2022-05-03 13:30:12 +0800

    Fix crystal use crystalline as LSP server.

--- plugins/crystal-mode/lsp-crystal_init.el
+++ plugins/crystal-mode/lsp-crystal_init.el
@@ -1,5 +1,7 @@
 (require 'lsp-mode_init)
-(require 'lsp-crystal)
+
+;; 注意，这里不要首先 require lsp-crystal, 否则将用默认的 scry 作为 servere.
+(setq lsp-clients-crystal-executable '("crystalline" "--stdio"))
 
 (defun zw/lsp-crystal-common-hooks ()
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
