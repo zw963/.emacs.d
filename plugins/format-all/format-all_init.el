ca9620842fd6b915d8dd3693f291d82bbd0e79ac
Billy.Zheng <vil963@gmail.com>
2022-07-02 14:16:29 +0800

    Add format_all and reformatter. (wip)

---
+++ plugins/format-all/format-all_init.el
@@ -0,0 +1,10 @@
+(require 'format-all)
+
+;; (add-hook 'mint-mode-hook (lambda ()
+;;                             (format-all-mode 1)
+;;                             (local-set-key [(control c) (control c)] 'format-all-buffer)
+;;                             ))
+
+(provide 'format-all_init)
+
+;;; format-all_init.el ends here
--- plugins/reformatter/reformatter_init.el
+++ plugins/reformatter/reformatter_init.el
@@ -1,19 +1,5 @@
 (require 'reformatter)
 
-(with-eval-after-load 'dart-mode
-  (reformatter-define dart-format
-                      :program "dart"
-                      :args '("format"))
-  ;; 自动定义了以下方法:
-  ;; dart-format-buffer dart-format-region
-  ;; 以及一个 minor-mode
-  ;; dart-format-on-save-mode
-  (add-hook 'dart-mode-hook (lambda ()
-                               (dart-format-on-save-mode 1)
-                               (define-key dart-mode-map (kbd "C-c C-c") 'dart-format-buffer)
-                              ))
-  )
-
 (provide 'reformatter_init)
 
 ;;; reformatter_init.el ends here
