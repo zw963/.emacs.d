9a82cb638be36cd973c0b4d257befcd580b3b32a
Billy.Zheng <vil963@gmail.com>
2022-08-07 15:02:56 +0800

    Add crystal lsp config.

--- plugins/crystal-mode/lsp-crystal_init.el
+++ plugins/crystal-mode/lsp-crystal_init.el
@@ -1,6 +1,13 @@
 ;; 注意，这里不要首先 require lsp-crystal, 否则将用默认的 scry 作为 server.
 (setq lsp-clients-crystal-executable '("crystalline" "--stdio"))
 
+;; (with-eval-after-load 'lsp-mode
+;;   (lsp-register-client
+;;    (make-lsp-client :new-connection (lsp-stdio-connection '("crystalline"))
+;;                     :activation-fn (lsp-activate-on "crystal")
+;;                     :priority '1
+;;                     :server-id 'crystalline)))
+
 (require 'lsp-mode_init)
 
 (defun zw/lsp-crystal-common-hooks ()
