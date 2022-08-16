10cdd930ac72646ff6643749027c7b3fc908a2e4
Billy.Zheng <vil963@gmail.com>
2022-08-16 22:41:28 +0800

    Enable crystal-mode format before save.

--- plugins/crystal-mode/crystal-mode_init.el
+++ plugins/crystal-mode/crystal-mode_init.el
@@ -7,6 +7,12 @@
 ;; (require 'flycheck-ameba)
 ;; (flycheck-ameba-setup)
 
+(add-hook 'crystal-mode-hook
+          (lambda ()
+            ;; 如果开启了 lsp 里面的 lsp-format-buffer, 则关闭这个。
+            (add-hook 'before-save-hook #'crystal-tool-format nil 'local)
+            ))
+
 (provide 'crystal-mode_init)
 
 ;;; crystal-mode_init.el ends here
