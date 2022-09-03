7351d428fa7891ab5f3413008d24aee231400e9a
Billy.Zheng <vil963@gmail.com>
2022-09-04 01:24:50 +0800

    config dumb-jump

--- plugins/dumb-jump/dumb-jump_init.el
+++ plugins/dumb-jump/dumb-jump_init.el
@@ -2,8 +2,23 @@
 
 (setq dumb-jump-force-searcher 'rg)
 
-(add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
-(setq xref-show-definitions-function #'xref-show-definitions-completing-read)
+(defun set-dumb-jump-as-default ()
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate 90 t)
+  (setq-local xref-show-definitions-function #'xref-show-definitions-completing-read)
+  )
+
+;; (add-hook 'crystal-mode-hook 'set-dumb-jump-as-default)
+(add-hook 'prog-mode-hook 'set-dumb-jump-as-default)
+
+(defhydra dumb-jump-hydra (:color blue :columns 3)
+    "Dumb Jump"
+    ("j" dumb-jump-go "Go")
+    ("o" dumb-jump-go-other-window "Other window")
+    ("e" dumb-jump-go-prefer-external "Go external")
+    ("x" dumb-jump-go-prefer-external-other-window "Go external other window")
+    ("i" dumb-jump-go-prompt "Prompt")
+    ("l" dumb-jump-quick-look "Quick look")
+    ("b" dumb-jump-back "Back"))
 
 (provide 'dumb-jump_init)
 
