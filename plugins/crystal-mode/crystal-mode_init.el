625f76bac7f53509389d178893249e255d7d893a
Billy.Zheng <vil963@gmail.com>
2022-09-19 11:16:28 +0800

    Add several useful command

--- plugins/crystal-mode/crystal-mode_init.el
+++ plugins/crystal-mode/crystal-mode_init.el
@@ -11,6 +11,9 @@
           (lambda ()
             ;; 如果开启了 lsp 里面的 lsp-format-buffer, 则关闭这个。
             (add-hook 'before-save-hook #'crystal-tool-format nil 'local)
+            (define-key crystal-mode-map [(meta \.) ] #'crystal-tool-imp)
+            (define-key crystal-mode-map [(control meta \.) ] #'crystal-tool-context)
+            (define-key crystal-mode-map [(control t) ] #'crystal-spec-switch)
             ))
 
 (provide 'crystal-mode_init)
