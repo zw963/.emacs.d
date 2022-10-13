734f94875e430f3f4a1e44e9cc9b7c8f650c9aed
Billy.Zheng <vil963@gmail.com>
2022-10-13 16:34:27 +0800

    toml-mode autoload

--- plugins/rust/rust_init.el
+++ plugins/rust/rust_init.el
@@ -1,8 +1,11 @@
 (require 'rust-mode_init)
+;; (require 'rustic_init)
 (require 'lsp-rust-mode_init)
 ;; (require 'rust-mode-eglot_init)
 
-;; (require 'rustic_init)
+(autoload 'toml-mode "toml-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.toml\\'" . toml-mode))
+(add-to-list 'auto-mode-alist '("\\`\\(rust-toolchain\\)\\'" . toml-mode))
 
 (provide 'rust_init)
 
