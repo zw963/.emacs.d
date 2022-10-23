b2e89497fa7060ce18ee46189d9f3dce4217cb48
Billy.Zheng <vil963@gmail.com>
2022-10-23 17:39:16 +0800

    remap easy-mark

--- plugins/easy-kill/easy-kill_init.el
+++ plugins/easy-kill/easy-kill_init.el
@@ -3,6 +3,7 @@
 ;; (global-set-key [remap mark-sexp] #'easy-mark)
 ;; (global-set-key [(control meta space)] #'easy-mark)
 (global-set-key [(meta w)] 'easy-kill)
+(global-set-key [remap mark-sexp] 'easy-mark)
 
 (provide 'easy-kill_init)
 ;;; easy-kill_init.el ends here
