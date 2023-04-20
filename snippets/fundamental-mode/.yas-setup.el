e6489a410e5311ec8ba49442bf63ceb301a40bc2
Billy.Zheng <vil963@gmail.com>
2023-04-20 22:59:36 +0800

    Fix bug.

--- snippets/fundamental-mode/.yas-setup.el
+++ snippets/fundamental-mode/.yas-setup.el
@@ -99,7 +99,7 @@ not-exist-after-field-delimiter：delimiter 有时候可能是只有前面有，
       (insert
        (concat
         (or field-seperator (_yas-field-seperator))
-        (_yas-expandfunc before-field-delimiter nil before-field-delimiter-function)
+        (_yas-expand-func before-field-delimiter nil before-field-delimiter-function)
         (yas-stripped-selected-text "" "" "")
         (unless not-exist-after-field-delimiter
           (_yas-expand-func after-field-delimiter nil after-field-delimiter-function))
