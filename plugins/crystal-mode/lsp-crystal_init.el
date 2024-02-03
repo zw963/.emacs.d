ec8efac69754d194d9be75eac05a0b282f9f9d8a
Billy.Zheng <vil963@gmail.com>
2024-02-03 23:22:56 +0800

    Try enable lsp for Crystal

--- plugins/crystal-mode/lsp-crystal_init.el
+++ plugins/crystal-mode/lsp-crystal_init.el
@@ -1,5 +1,5 @@
 ;; 注意，这里不要首先 require lsp-crystal, 否则将用默认的 scry 作为 server.
-(setq lsp-clients-crystal-executable '("crystalline" "--stdio"))
+;; (setq lsp-clients-crystal-executable '("crystalline" "--stdio"))
 
 ;; (with-eval-after-load 'lsp-mode
 ;;   (lsp-register-client
@@ -7,6 +7,15 @@
 ;;                     :activation-fn (lsp-activate-on "crystal")
 ;;                     :priority '1
 ;;                     :server-id 'crystalline)))
+
+(with-eval-after-load 'lsp-mode
+  (add-to-list 'lsp-language-id-configuration
+               '(crystal-mode . "crystal"))
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection '("crystalline"))
+                    :activation-fn (lsp-activate-on "crystal")
+                    :priority '1
+                    :server-id 'crystalline)))
 
 (require 'lsp-mode_init)
 
