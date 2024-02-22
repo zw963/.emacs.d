df956660187062980a31b43b35708139b8cafedc
Billy.Zheng <vil963@gmail.com>
2024-02-22 19:00:57 +0800

    disable ruby-mode in apheleia

--- plugins/apheleia/apheleia_init.el
+++ plugins/apheleia/apheleia_init.el
@@ -1,6 +1,6 @@
 (require 'apheleia)
 
-(add-to-list 'apheleia-mode-alist '(enh-ruby-mode . prettier-ruby))
+;; (add-to-list 'apheleia-mode-alist '(enh-ruby-mode . prettier-ruby))
 ;; (add-to-list 'apheleia-formatters '(crystal-format . ("crystal" "tool" "format" filepath)))
 ;; (add-to-list 'apheleia-mode-alist '(crystal-mode . crystal-format))
 
@@ -11,8 +11,8 @@
       (delete '(js3-mode . prettier-javascript)
               (delete '(js-mode . prettier-javascript)
                       (delete '(web-mode . prettier)
-                              apheleia-mode-alist))))
-
+                              (delete '(ruby-mode . prettier-ruby)
+                                      apheleia-mode-alist)))))
 (setq apheleia-log-only-errors nil)
 
 (add-hook 'prog-mode-hook 'apheleia-global-mode)
