f02fa4ed3795e398cf32d4c7c8c8b3cefa80b7b3
Billy.Zheng <vil963@gmail.com>
2024-03-10 23:29:34 +0800

    update window.el

--- windows.el
+++ windows.el
@@ -1,50 +1,50 @@
 ;; -------------------------- 中英文字体对齐 -----------------------------
 ;; 下面的代码，用来在放大字体时，保持英文中文比例一致，让表格总是对齐
 
-(defun qiang-font-existsp (font)
-  (if (null (x-list-fonts font))
-      nil
-    t))
+;; (defun qiang-font-existsp (font)
+;;   (if (null (x-list-fonts font))
+;;       nil
+;;     t))
 
-(defun qiang-make-font-string (font-name font-size)
-  (if (and (stringp font-size)
-           (equal ":" (string (elt font-size 0))))
-      (format "%s%s" font-name font-size)
-    (format "%s %s" font-name font-size)))
+;; (defun qiang-make-font-string (font-name font-size)
+;;   (if (and (stringp font-size)
+;;            (equal ":" (string (elt font-size 0))))
+;;       (format "%s%s" font-name font-size)
+;;     (format "%s %s" font-name font-size)))
 
-(defvar zh-font-list '("LXGW WenKai Mono" "HanaMinB"))
+;; (defvar zh-font-list '("LXGW WenKai Mono" "HanaMinB"))
 ;; (defvar en-font-list '("Fira Code Retina" "Iosevka" ))
-(defvar en-font-list '("IBM Plex Mono" "Iosevka" ))
+;; (defvar en-font-list '("IBM Plex Mono" "Iosevka" ))
 
-(defun qiang-set-font (english-fonts
-                       english-font-size
-                       chinese-fonts
-                       &optional chinese-font-scale)
+;; (defun qiang-set-font (english-fonts
+;;                        english-font-size
+;;                        chinese-fonts
+;;                        &optional chinese-font-scale)
 
-  (setq chinese-font-scale (or chinese-font-scale 1.2))
+;;   (setq chinese-font-scale (or chinese-font-scale 1.2))
 
-  (setq face-font-rescale-alist
-        (cl-loop for x in zh-font-list
-                 collect (cons x chinese-font-scale)))
+;;   (setq face-font-rescale-alist
+;;         (cl-loop for x in zh-font-list
+;;                  collect (cons x chinese-font-scale)))
 
-  "english-font-size could be set to \":pixelsize=18\" or a integer.
-If set/leave chinese-font-scale to nil, it will follow english-font-size"
+;;   "english-font-size could be set to \":pixelsize=18\" or a integer.
+;; If set/leave chinese-font-scale to nil, it will follow english-font-size"
 
-  (let ((en-font (qiang-make-font-string
-                  (cl-find-if #'qiang-font-existsp english-fonts)
-                  english-font-size))
-        (zh-font (font-spec :family (cl-find-if #'qiang-font-existsp chinese-fonts))))
+;;   (let ((en-font (qiang-make-font-string
+;;                   (cl-find-if #'qiang-font-existsp english-fonts)
+;;                   english-font-size))
+;;         (zh-font (font-spec :family (cl-find-if #'qiang-font-existsp chinese-fonts))))
 
-    ;; Set the default English font
-    (message "Set English Font to %s" en-font)
-    (set-face-attribute 'default nil :font en-font)
+;;     ;; Set the default English font
+;;     (message "Set English Font to %s" en-font)
+;;     (set-face-attribute 'default nil :font en-font)
 
-    ;; Set Chinese font
-    ;; Do not use 'unicode charset, it will cause the English font setting invalid
-    (message "Set Chinese Font to %s" zh-font)
-    (dolist (charset '(kana han symbol cjk-misc bopomofo))
-      (set-fontset-font (frame-parameter nil 'font)
-                        charset zh-font))))
+;;     ;; Set Chinese font
+;;     ;; Do not use 'unicode charset, it will cause the English font setting invalid
+;;     (message "Set Chinese Font to %s" zh-font)
+;;     (dolist (charset '(kana han symbol cjk-misc bopomofo))
+;;       (set-fontset-font (frame-parameter nil 'font)
+;;                         charset zh-font))))
 
 ;; ------------------------------ 启动最大化 ------------------------------
 ;; 可以通过运行 fc-list 来查看字体的名称.
