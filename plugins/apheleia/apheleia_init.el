bf84e25926a198e0c35a4896922430e4ae9c747a
Billy.Zheng <vil963@gmail.com>
2024-03-11 23:55:11 +0800

    Fix apheleia mix-format

--- plugins/apheleia/apheleia_init.el
+++ plugins/apheleia/apheleia_init.el
@@ -3,10 +3,13 @@
 ;; (add-to-list 'apheleia-mode-alist '(enh-ruby-mode . prettier-ruby))
 ;; (add-to-list 'apheleia-formatters '(crystal-format . ("crystal" "tool" "format" filepath)))
 ;; (add-to-list 'apheleia-mode-alist '(crystal-mode . crystal-format))
-(add-to-list 'apheleia-mode-alist '(elixir-ts-mode . mix-format))
 
 (setf (alist-get 'dart-format apheleia-formatters)
       '("flutter" "format" filepath))
+
+(add-to-list 'apheleia-mode-alist '(elixir-ts-mode . mix-format))
+(setf (alist-get 'mix-format apheleia-formatters)
+      '("mix" "format" filepath))
 
 (setq apheleia-mode-alist
       (delete '(js3-mode . prettier-javascript)
@@ -15,9 +18,9 @@
                               (delete '(ruby-mode . prettier-ruby)
                                       apheleia-mode-alist)))))
 
-;; (setq apheleia-log-only-errors nil)
+(setq apheleia-log-only-errors nil)
 
-(add-hook 'prog-mode-hook 'apheleia-global-mode)
+(add-hook 'prog-mode-hook 'apheleia-mode)
 
 (provide 'apheleia_init)
 
