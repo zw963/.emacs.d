71625f3a7fec4e2a90f4f5b76292002f4ae86022
Billy.Zheng <vil963@gmail.com>
2024-04-15 02:05:10 +0800

    move haskell-mode into backup

--- plugins/haskell/haskell-mode_init.el
+++
@@ -1,7 +0,0 @@
-(require 'haskell-mode-autoloads)
-
-;; (add-to-list 'Info-default-directory-list "~/lib/emacs/haskell-mode/")
-
-(provide 'haskell-mode_init)
-
-;;; haskell-mode_init.el ends here
--- plugins/haskell/lsp-haskell_init.el
+++
@@ -1,8 +0,0 @@
-(require 'lsp-haskell)
-
-(add-hook 'haskell-mode-hook #'lsp-mode-common-hooks)
-(add-hook 'haskell-literate-mode-hook #'lsp-mode-common-hooks)
-
-(provide 'lsp-haskell_init)
-
-;;; lsp-haskell_init.el ends here
