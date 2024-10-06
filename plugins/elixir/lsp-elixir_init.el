7bc33c4eac544a6aa9227aa4b1c3f672d68046cc
Billy.Zheng <vil963@gmail.com>
2024-10-06 20:49:23 +0800

    update elixir lsp

--- plugins/elixir/lsp-elixir_init.el
+++ plugins/elixir/lsp-elixir_init.el
@@ -1,5 +1,5 @@
 (require 'lsp-mode_init)
-(setq lsp-elixir-ls-version "v0.20.0")
+(setq lsp-elixir-ls-version "v0.23.0")
 (require 'lsp-elixir)
 
 (add-hook 'elixir-ts-mode-hook 'lsp-mode-common-hooks)
