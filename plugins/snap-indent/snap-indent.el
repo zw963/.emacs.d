f8400dfa9ccc236017ee74873258aae1656c6424
Billy.Zheng <vil963@gmail.com>
2025-03-25 22:45:40 +0800

    update snap-indent

--- plugins/snap-indent/snap-indent.el
+++ plugins/snap-indent/snap-indent.el
@@ -1,11 +1,11 @@
 ;;; snap-indent.el --- Simple automatic indentation -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022 Jeff Valk
+;; Copyright (C) 2023 Jeff Valk
 
 ;; Author: Jeff Valk <jv@jeffvalk.com>
 ;; URL: https://github.com/jeffvalk/snap-indent
 ;; Keywords: indent tools convenience
-;; Version: 1.0
+;; Version: 1.1
 ;; Package-Requires: ((emacs "24.1"))
 
 ;; This file is NOT part of GNU Emacs.
@@ -37,10 +37,17 @@
 ;; - When indenting, additionally format text, e.g. tabify, untabify, remove
 ;;   trailing whitespace, etc (optional)
 ;; - Prevent minor mode activation in certain major modes (optional)
+;; - Skip indentation systematically by maximum text length or according to any
+;;   user-defined predicate (optional)
+;; - Skip indentation for a single operation using an argument prefix (optional)
 ;;
 ;; Snap-indent's additional formatting behavior is very flexible. Any function
 ;; that operates on a region may be used, and multiple functions may be
 ;; specified.
+;;
+;; Snap-indent can be configured to skip indentation with equal flexibility.
+;; Any predicate function can be set control this behavior systematically, and
+;; indentation may be suppressed for a single operation with key input.
 
 ;;; Code:
 
@@ -49,13 +56,24 @@
   :prefix "snap-indent-"
   :group 'indent)
 
-(defcustom snap-indent-excluded-modes '(elm-mode
+(defcustom snap-indent-excluded-modes '(cmake-ts-mode
+                                        coffee-mode
+                                        conf-mode
+                                        elm-mode
+                                        haml-mode
                                         haskell-mode
+                                        makefile-automake-mode
+                                        makefile-bsdmake-mode
                                         makefile-gmake-mode
+                                        makefile-imake-mode
+                                        makefile-makepp-mode
                                         makefile-mode
                                         occam-mode
                                         python-mode
-                                        yaml-mode)
+                                        python-ts-mode
+                                        slim-mode
+                                        yaml-mode
+                                        yaml-ts-mode)
   "Modes in which `snap-indent-mode' should not be activated.
 This permits activation for `prog-mode' while excluding certain modes derived
 from `prog-mode', for example."
@@ -76,6 +94,31 @@ tab/space conversion and `delete-trailing-whitespace'."
   :type '(choice
           (function :tag "Single function")
           (repeat :tag "List of functions" function))
+  :group 'snap-indent)
+
+(defcustom snap-indent-length-limit nil
+  "Maximum text length to indent.
+Set this to prevent any performance issues with large blocks of text.
+When nil, no limit is applied."
+  :type 'integer
+  :group 'snap-indent)
+
+(defcustom snap-indent-skip-on-prefix-arg nil
+  "Whether a prefix command argument causes indentation to be skipped.
+When non-nil, this lets you skip indentation for a single operation without
+disabling `snap-indent-mode'."
+  :type 'boolean
+  :group 'snap-indent)
+
+(defcustom snap-indent-skip-on-condition nil
+  "Predicate function to cause indentation to be skipped.
+When specified, this lets you skip indentation for a single operation without
+disabling `snap-indent-mode' according to any logic you choose.
+
+The function must accept two arguments, which specify the start and end
+positions of the region on which to (potentially) operate. The function should
+return non-nil to skip indentation, and nil otherwise."
+  :type 'function
   :group 'snap-indent)
 
 ;; To make user configuration more expressive and less error-prone,
@@ -100,17 +143,27 @@ tab/space conversion and `delete-trailing-whitespace'."
       (let ((end* (+ end (- (point-max) orig-max)))) ; account for prior changes
         (funcall format beg end*)))))
 
+(defun snap-indent-maybe-indent (beg end)
+  "If the region between BEG and END should be indented, dispatch that action."
+  (unless (or (and snap-indent-length-limit
+                   (> (- end beg) snap-indent-length-limit))
+              (and snap-indent-skip-on-prefix-arg
+                   current-prefix-arg)
+              (and snap-indent-skip-on-condition
+                   (funcall snap-indent-skip-on-condition beg end)))
+    (snap-indent-indent beg end)))
+
 (defun snap-indent-save-handler ()
   "Indent buffer text on save as specified."
   (when snap-indent-on-save
-    (snap-indent-indent (point-min) (point-max))))
+    (snap-indent-maybe-indent (point-min) (point-max))))
 
 (defun snap-indent-command-handler ()
   "Indent region text on yank."
   (when (memq this-command '(yank yank-pop))
-    (snap-indent-indent (region-beginning) (region-end))))
+    (snap-indent-maybe-indent (region-beginning) (region-end))))
 
-;;;###Autoload
+;;;###autoload
 (define-minor-mode snap-indent-mode
   "Toggle snap-indent mode on or off.
 Turn snap-indent on if ARG is positive, or off otherwise."
