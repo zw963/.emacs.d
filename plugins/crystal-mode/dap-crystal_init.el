954c54326932ace3b496b553cb4bacf33efb0052
Billy.Zheng <vil963@gmail.com>
2025-03-25 17:34:42 +0800

    disable dap when start crystal-mode and go-mode

---
+++ plugins/crystal-mode/dap-crystal_init.el
@@ -0,0 +1,20 @@
+(require 'dap-mode_init)
+运行 dap-codelldb-setup 来安装扩展
+(require 'dap-codelldb)
+
+(dap-register-debug-template "Crystal LLDB"
+                             (list :type "lldb"
+                                   :request "launch"
+                                   :name "crystal: debug current file"
+                                   :preLaunchTask "crystal: build current file (debug)"
+                                   :mode "auto"
+                                   :program "${workspaceFolder}/bin/${fileBasenameNoExtension}"
+                                   :cwd "${workspaceFolder}"
+                                   :buildFlags nil
+                                   :initCommands: [
+                                                   "command script import /home/zw963/Crystal/crystal-lang/crystal/etc/lldb/crystal_formatters.py"
+                                                   ]
+                                   :args nil
+                                   :env nil))
+
+(provide 'dap-crystal_init)
--- plugins/crystal-mode/lsp-crystal_init.el
+++ plugins/crystal-mode/lsp-crystal_init.el
@@ -20,24 +20,7 @@
 (require 'lsp-mode_init)
 (add-hook 'crystal-mode-hook 'lsp-mode-common-hooks)
 
-(require 'dap-mode_init)
-;; 运行 dap-codelldb-setup 来安装扩展
-(require 'dap-codelldb)
-
-(dap-register-debug-template "Crystal LLDB"
-                             (list :type "lldb"
-                                   :request "launch"
-                                   :name "crystal: debug current file"
-                                   :preLaunchTask "crystal: build current file (debug)"
-                                   :mode "auto"
-                                   :program "${workspaceFolder}/bin/${fileBasenameNoExtension}"
-                                   :cwd "${workspaceFolder}"
-                                   :buildFlags nil
-                                   :initCommands: [
-                                                   "command script import /home/zw963/Crystal/crystal-lang/crystal/etc/lldb/crystal_formatters.py"
-                                                   ]
-                                   :args nil
-                                   :env nil))
+;; (require 'dap-crystal_init)
 
 (provide 'lsp-crystal_init)
 
