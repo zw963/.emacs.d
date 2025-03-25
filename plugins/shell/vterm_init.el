c84607db4ba8195ab35b78c74587c0cbc4e38508
Billy.Zheng <vil963@gmail.com>
2025-03-26 01:05:37 +0800

    move vterm and shell into same folder

--- plugins/shell/vterm_init.el
+++ plugins/shell/vterm_init.el
@@ -64,11 +64,30 @@
             (buffer-face-mode t)))
 
 (require 'eterm-256color)
-
 (setq vterm-term-environment-variable "eterm-color")
 
 ;; 这个不用了，使用 popper.el 管理。
 (require 'multi-vterm_init)
+
+;; shell config
+
+(custom-set-variables
+ '(comint-scroll-to-bottom-on-input t)
+ '(comint-completion-autolist t)     ; show completion list when ambiguous
+ '(comint-input-ignoredups t)           ; no duplicates in command history
+ '(comint-completion-addsuffix nil)       ; insert space/slash after file completion
+ '(comint-buffer-maximum-size 20000)    ; max length of the buffer in lines
+ '(comint-prompt-read-only t)           ; if this is t, it breaks shell-command
+ '(comint-get-old-input (lambda () ""))      ; what to run when i press enter on a
+                                        ; line above the current prompt
+ )
+
+(add-hook 'shell-mode-hook (lambda ()
+                             (ansi-color-for-comint-mode-on)
+                             (define-key shell-mode-map [(control n)] 'comint-next-input)
+                             (define-key shell-mode-map [(control p)] 'comint-previous-input)
+                             (define-key shell-mode-map [(control r)] 'comint-previous-matching-input-from-input)
+                             ))
 
 (provide 'vterm_init)
 
