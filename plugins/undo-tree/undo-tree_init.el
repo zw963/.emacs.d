59fedcfea1735bad05c919cf1f84415bf1779b1f
Billy.Zheng <vil963@gmail.com>
2025-03-27 00:58:50 +0800

    undo tree disable undo in region

--- plugins/undo-tree/undo-tree_init.el
+++ plugins/undo-tree/undo-tree_init.el
@@ -15,7 +15,7 @@
    undo-tree-auto-save-history t
    ;; undo-tree-visualizer-diff t  ; 这个无需设定, d 快捷键将会显示 diff
    ;; undo-tree-visualizer-timestamps t ; 则个可以通过 t 切换.
-   undo-tree-enable-undo-in-region t ; 不需要选区内 undo
+   undo-tree-enable-undo-in-region nil ; 不需要选区内 undo
    )
 
   ;; 使用这个目录，可能是造成 undo 性能问题的元凶。
