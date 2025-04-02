3159f1ae1935fc910e4228a10d0a402e2d165665
Billy.Zheng <vil963@gmail.com>
2025-04-02 20:36:10 +0800

    Added indent-bars mode

---
+++ plugins/indent-bars/indent-bars_init.el
@@ -0,0 +1,27 @@
+(require 'indent-bars-ts)
+
+(setq indent-bars-treesit-support t)
+
+(dolist (hook '(coffee-mode-hook
+                feature-mode-hook
+                yaml-mode-hook
+                yaml-ts-mode-hook
+                python-mode-hook
+                dart-mode-hook
+                slim-mode-hook
+                web-mode-hook
+                crystal-mode-hook
+                bash-ts-mode-hook
+                ))
+  (add-hook hook 'indent-bars-mode))
+
+
+;; (dolist (hook '(
+;;                 yaml-ts-mode-hook
+;;                 bash-ts-mode-hook
+;;                 ))
+;;   (add-hook hook 'indent-bars-mode))
+
+(provide 'indent-bars_init)
+
+;;; indent-bars_init.el ends here
