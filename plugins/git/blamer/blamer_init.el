465ba6b99eb9c8a8616e5fe9134dbb7e0d7de9e4
Billy.Zheng <vil963@gmail.com>
2025-04-21 19:21:49 +0800

    Added git-messenger back

--- plugins/git/blamer/blamer_init.el
+++ plugins/git/blamer/blamer_init.el
@@ -1,15 +1,19 @@
 (require 'blamer)
+(require 'git-messenger)
 
-(setq blamer-show-avatar-p nil)
+(setq git-messenger:show-detail t)
+(setq popup-enable-display-line-number-mode-p t)
+(global-set-key [(control x) (v) (m)] 'git-messenger:popup-message)
+
+;; (setq blamer-show-avatar-p nil)
 (setq blamer-idle-time 1)
 (setq blamer-max-lines 1)
 
-(global-set-key [(control x) (v) (m)] 'blamer-show-posframe-commit-info)
-
-(setq blamer-face '((t :foreground "#7a88cf"
-                       :background nil
-                       :height 140
-                       :italic t)))
+;; (global-set-key [(control x) (v) (m)] 'blamer-show-posframe-commit-info)
+;; (setq blamer-face '((t :foreground "#7a88cf"
+;;                        :background nil
+;;                        :height 140
+;;                        :italic t)))
 
 (global-blamer-mode 1)
 
