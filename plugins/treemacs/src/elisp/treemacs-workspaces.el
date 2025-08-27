472bf24f6bca90746b1035483a183d6af08be098
Billy.Zheng <vil963@gmail.com>
2025-08-28 01:25:20 +0800

    update treemacs

--- plugins/treemacs/src/elisp/treemacs-workspaces.el
+++ plugins/treemacs/src/elisp/treemacs-workspaces.el
@@ -139,16 +139,17 @@ If no workspace is assigned to the current scope the persisted workspaces will
 be loaded and a workspace will be found based on the `current-buffer'.
 
 This function can be used with `setf'."
+  (treemacs--maybe-load-workspaces)
   (or treemacs-override-workspace
-      (-if-let (shelf (treemacs-current-scope-shelf))
-          (treemacs-scope-shelf->workspace shelf)
-        (treemacs--maybe-load-workspaces)
-        (let* ((workspace (treemacs--find-workspace (buffer-file-name (current-buffer))))
-               (new-shelf (treemacs-scope-shelf->create! :workspace workspace)))
-          (setf (treemacs-current-scope-shelf) new-shelf)
-          (run-hook-with-args treemacs-workspace-first-found-functions
-                              workspace (treemacs-current-scope))
-          workspace))))
+      (let* ((shelf (treemacs-current-scope-shelf))
+             (workspace (and shelf (treemacs-scope-shelf->workspace shelf))))
+        (or workspace
+            (let* ((workspace (treemacs--find-workspace (buffer-file-name (current-buffer))))
+                   (new-shelf (treemacs-scope-shelf->create! :workspace workspace)))
+              (setf (treemacs-current-scope-shelf) new-shelf)
+              (run-hook-with-args treemacs-workspace-first-found-functions
+                                  workspace (treemacs-current-scope))
+              workspace)))))
 
 (gv-define-setter treemacs-current-workspace (val)
   `(let ((shelf (treemacs-current-scope-shelf)))
@@ -175,7 +176,6 @@ PATH: String"
                ('always-ask
                 (treemacs--select-workspace-by-name))))))))
 
-;; TODO(2020/11/25): NAME
 (define-inline treemacs--find-project-for-buffer (&optional buffer-file)
   "In the current workspace find the project current buffer's file falls under.
 Optionally supply the BUFFER-FILE in case it is not available by calling the
@@ -702,7 +702,8 @@ Returns t when the name is invalid.
 
 NAME: String"
   (declare (pure t) (side-effect-free t))
-  (or (s-blank-str? name)
+  (or (null name)
+      (s-blank-str? name)
       (s-contains? "\n" name)
       (not (s-matches? (rx (1+ (or space (syntax word) (syntax symbol) (syntax punctuation)))) name))))
 
