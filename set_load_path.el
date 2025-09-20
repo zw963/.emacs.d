5d674e5482708ba87c6624c986ccea964219f4b6
Billy.Zheng <vil963@gmail.com>
2025-09-21 03:00:36 +0800

    Added auto_loads back

--- set_load_path.el
+++ set_load_path.el
@@ -4,7 +4,7 @@
 
 ;;;; ------------------------------ settings load path ------------------------------
 (setq default-directory (file-name-directory (or load-file-name buffer-file-name)))
-;; (normal-top-level-add-to-load-path '("autoload_plugins/"))
+(normal-top-level-add-to-load-path '("autoload_plugins/"))
 (let ((default-directory (concat default-directory "plugins")))
   (normal-top-level-add-subdirs-to-load-path)
   (add-to-list 'load-path default-directory))
