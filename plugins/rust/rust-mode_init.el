5debbe74bedfb92d1995274a9d33cfcfbdca4373
Billy.Zheng <vil963@gmail.com>
2025-12-14 22:14:49 +0800

    split rust-mode_init.el

--- plugins/rust/rust-mode_init.el
+++ plugins/rust/rust-mode_init.el
@@ -4,6 +4,30 @@
 
 ;; (setq rust-format-on-save t)
 
+(add-hook 'rust-mode-hook
+          #'(lambda ()
+              (setq-local company-idle-delay 0.2)
+              (setq-local company-minimum-prefix-length 3)
+              (local-set-key [(control c) (control c)] 'rust-format-buffer)
+              (local-set-key [(control c) (return)] 'rust-run)
+              (local-set-key [(control c) (control t)] 'rust-test)
+              (local-set-key [(control c) (tab)] 'rust-compile)
+              ))
+
+(with-eval-after-load 'dap-mode
+  (dap-register-debug-template "Rust::GDB Run Configuration"
+                               (list :type "gdb"
+                                     :request "launch"
+                                     :name "GDB::Run"
+                                     :gdbpath "rust-gdb"
+                                     :target nil
+                                     :cwd nil))
+  )
+
+;; (require 'cargo_init)
+
+;; Rust 里 ' 既用于 字符字面量（'x'），也用于 生命周期（'a / &'a T）。
+;; 你显然是想让它在写字符时帮你补齐，但别在写生命周期时乱插一个多余的 '。
 (defvar electric-pair-inhibit-predicate-mode-chars-alist
   '((t . nil))
   "A list of major-mode and inhibit chars.
@@ -54,58 +78,6 @@ CHAR-FUNCTION
 (add-to-list 'electric-pair-inhibit-predicate-mode-chars-alist
              '(rust-mode . ((?' . "\&'"))))
 (modify-syntax-entry ?' "\"" rust-mode-syntax-table)
-
-(add-hook 'rust-mode-hook
-          #'(lambda ()
-              (setq-local company-idle-delay 0.2)
-              (setq-local company-minimum-prefix-length 3)
-              (local-set-key [(control c) (control m)] 'rust-format-buffer)
-              (local-set-key [(control c) (return)] 'rust-run)
-              (local-set-key [(control c) (tab)] 'rust-compile)
-              ))
-
-;; 需要安装的依赖：
-;; cargo install audit cargo-outdated
-(require 'cargo)
-(with-eval-after-load 'cargo
-  (setq compilation-ask-about-save nil)   ; 运行 cargo 编译前，自动保存项目内所有文件。
-  (define-key cargo-process-mode-map [(n)] 'compilation-next-error)
-  (define-key cargo-process-mode-map [(p)] 'compilation-previous-error)
-  (define-key cargo-process-mode-map [(control \8)] 'quit-window)
-
-  (add-hook 'rust-mode-hook
-            #'(lambda ()
-                ;; (define-key cargo-minor-mode-map (kbd "C-c C-e") 'cargo-process-bench)
-                ;; (define-key cargo-minor-mode-map (kbd "C-c C-x") 'cargo-process-run-example)
-                (local-set-key (kbd "C-c C-a") 'cargo-process-add)
-                (local-set-key (kbd "C-c C-S-a") 'cargo-process-audit)
-                (local-set-key (kbd "C-c C-s") 'cargo-process-search)
-                (local-set-key (kbd "C-c C-o") 'cargo-process-doc-open)
-                (local-set-key (kbd "C-c C-S-o") 'cargo-process-outdated)
-                (local-set-key (kbd "C-c C-S-d") 'cargo-process-rm)
-                (local-set-key (kbd "C-c C-u") 'cargo-process-update)
-                (local-set-key (kbd "C-c C-S-u") 'cargo-process-upgrade)
-                (local-set-key (kbd "C-c C-r") 'cargo-process-run)
-                (local-set-key (kbd "C-c <return>") 'cargo-process-run)
-                (local-set-key (kbd "C-c <tab>") 'cargo-process-build)
-                (local-set-key (kbd "C-c t") 'cargo-process-test)
-                (local-set-key (kbd "C-c C-c") 'cargo-process-repeat)
-                ))
-
-  (with-eval-after-load 'shackle
-    (add-to-list 'shackle-rules '(cargo-process-mode :select t :size 0.3 :autoclose t))
-    )
-  )
-
-(with-eval-after-load 'dap-mode
-  (dap-register-debug-template "Rust::GDB Run Configuration"
-                               (list :type "gdb"
-                                     :request "launch"
-                                     :name "GDB::Run"
-                                     :gdbpath "rust-gdb"
-                                     :target nil
-                                     :cwd nil))
-  )
 
 (provide 'rust-mode_init)
 ;;; rust-mode_init.el ends here
