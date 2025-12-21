5e06c6fe5f0cb09a8a760b177ffb8d6c282ebb0a
Billy.Zheng <vil963@gmail.com>
2025-12-22 02:13:45 +0800

    fix hotfuzz_init

--- plugins/hotfuzz/hotfuzz_init.el
+++ plugins/hotfuzz/hotfuzz_init.el
@@ -2,12 +2,16 @@
 
 (require 'hotfuzz)
 
-(setq completion-styles '(hotfuzz))
-(setq helm-completion-style 'emacs)
+(with-eval-after-load 'helm
+  (setq helm-completion-style 'emacs)
+  )
 
-(setq completion-category-defaults nil)
-(completion-category-overrides '((buffer (display-sort-function . identity))
-                                 (eglot-capf (styles hotfuzz))))
+(setq
+ completion-styles '(hotfuzz)
+ completion-category-defaults nil
+ completion-category-overrides '((buffer (display-sort-function . identity))
+                                 (eglot-capf (styles hotfuzz)))
+ )
 
 (provide 'hotfuzz_init)
 
