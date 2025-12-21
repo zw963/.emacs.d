dd3e37a98e472b7fdff88a74abd484bbf6016ad7
Billy.Zheng <vil963@gmail.com>
2025-12-21 23:54:07 +0800

    Fix smartparens config

--- plugins/smartparens/smartparens_init.el
+++ plugins/smartparens/smartparens_init.el
@@ -3,28 +3,36 @@
 (require 'smartparens-config)
 
 (sp-local-pair 'web-mode "<#" "#>")
+(sp-local-pair 'web-mode "<%" "%>")
 (sp-pair "`" "'" :when '(sp-in-comment-p))
 
+;; sp 有一个全局设定，如果单引号 ' 在一个 word 之后，就不会自动 pair
+;; 这里在 shell 模式将其关闭。
 (sp-local-pair 'sh-mode "'" "'" :unless nil)
-(sp-local-pair 'org-mode "=" nil :actions nil)
-(sp-local-pair 'org-mode "`" "`" :actions nil)
-(sp-local-pair 'org-mode "'" "'" :actions nil)
-(sp-local-pair 'org-mode "`" "'" :unless nil) ;; 这个不生效？
+(sp-local-pair 'bash-ts-mode "'" "'" :unless nil)
 
-(let ((pairs '(("{" nil) ("[" nil) ("(" nil))))
-  (mapc
-   (lambda (pair)
-     (sp-pair (-first-item pair)
-              (-last-item pair)
-              :post-handlers
-              '(:add ("||\n[i]" "RET"))))
-   pairs))
+(sp-local-pair 'org-mode "`" "'" :when nil :unless nil) ;; 这个不生效？
 
-;; https://github.com/rdallasgray/graphene/blob/master/lib/graphene-smartparens-config.el
-(sp-local-pair
- '(markdown-mode gfm-mode) "*" "*" :unless '(sp-in-string-p) :actions '(insert wrap))
+;; ;; 关闭 = 自动匹配的行为
+;; (sp-local-pair 'org-mode "=" nil :actions nil)
+
+;; (sp-local-pair 'org-mode "`" "'" :actions '(insert wrap))
+
+;; (let ((pairs '(("{" nil) ("[" nil) ("(" nil))))
+;;   (mapc
+;;    (lambda (pair)
+;;      (sp-pair (car pair)
+;;               (cadr pair)
+;;               :post-handlers
+;;               '(:add ("||\n[i]" "RET"))))
+;;    pairs))
+
+;; ;; https://github.com/rdallasgray/graphene/blob/master/lib/graphene-smartparens-config.el
+;; (sp-local-pair
+;;  '(markdown-mode gfm-mode) "*" "*" :unless '(sp-in-string-p) :actions '(insert wrap))
 
 ;; (add-hook 'prog-mode-hook #'smartparens-mode)
+
 (smartparens-global-mode)
 
 (provide 'smartparens_init)
