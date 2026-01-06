9df09bf66d92be3443b0ff5ae9e627f95cbca6ff
Billy.Zheng <vil963@gmail.com>
2026-01-07 00:32:07 +0800

    use diff-hl again

--- plugins/git/git_init.el
+++ plugins/git/git_init.el
@@ -3,8 +3,8 @@
 (require 'git-timemachine_init)
 
 ;; C-x v * 从第一个存在 diff 的 hunk 开始, 往下查看, 然后可以 n/p 来前进或后退 r 可以 revert, c 拷贝原始内容
-;; (require 'diff-hl_init)
-(require 'git-gutter_init)
+(require 'diff-hl_init)
+;; (require 'git-gutter_init)
 
 ;; (require 'blamer_init)
 
