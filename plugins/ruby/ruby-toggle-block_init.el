4f34fb2106c11737869f9f0e99e82c64db046bf2
Billy.Zheng <vil963@gmail.com>
2026-01-08 15:16:38 +0800

    fix ruby-toggle-block_init.el

--- plugins/ruby/ruby-toggle-block_init.el
+++ plugins/ruby/ruby-toggle-block_init.el
@@ -56,9 +56,16 @@ If the result is do-end block, it will always be multiline."
                    (> end start)))
             (if (match-beginning 1)
                 (progn
-                  (if (eql major-mode 'enh-ruby-mode)
-                      (enh-ruby-brace-to-do-end beg end)
+                  (cond
+                   ((member major-mode '(crystal-mode))
+                    (crystal-brace-to-do-end beg end))
+
+                   ((member major-mode '(ruby-mode ruby-ts-mode))
                     (ruby-brace-to-do-end beg end))
+
+                   ((member major-mode '(enh-ruby-mode))
+                    (enh-ruby-brace-to-do-end beg end))
+                   )
                   (end-of-line)
                   (if (next-line-empty-p)
                       (progn
@@ -71,70 +78,3 @@ If the result is do-end block, it will always be multiline."
 (provide 'ruby-toggle-block_init)
 
 ;;; ruby-toggle-block_init.el ends here.
-
-
-;; Copied from ruby-mode
-
-(defun ruby-brace-to-do-end (orig end)
-  (let (beg-marker end-marker)
-    (goto-char end)
-    (when (eq (char-before) ?\})
-      (delete-char -1)
-      (when (save-excursion
-              (let ((n (skip-chars-backward " \t")))
-                (if (< n 0) (delete-char (- n))))
-              (not (bolp)))
-        (insert "\n"))
-      (insert "end")
-      (setq end-marker (point-marker))
-      (when (and (not (eobp)) (eq (char-syntax (char-after)) ?w))
-        (insert " "))
-      (goto-char orig)
-      (delete-char 1)
-      (when (eq (char-syntax (char-before)) ?w)
-        (insert " "))
-      (insert "do")
-      (setq beg-marker (point-marker))
-      (when (looking-at "\\(\\s \\)*|")
-        (unless (match-beginning 1)
-          (insert " "))
-        (goto-char (1+ (match-end 0)))
-        (search-forward "|"))
-      (unless (looking-at "\\s *$")
-        (insert "\n"))
-      (indent-region beg-marker end-marker)
-      (goto-char beg-marker)
-      t)))
-
-(defun ruby-do-end-to-brace (orig end)
-  (let (beg-marker end-marker beg-pos end-pos)
-    (goto-char (- end 3))
-    (when (looking-at ruby-block-end-re)
-      (delete-char 3)
-      (setq end-marker (point-marker))
-      (insert "}")
-      (goto-char orig)
-      (delete-char 2)
-      (insert "{")
-      (if (looking-at "\\s +|")
-          (progn
-            (just-one-space (if ruby-toggle-block-space-before-parameters 1 0))
-            (setq beg-marker (point-marker))
-            (forward-char)
-            (re-search-forward "|" (line-end-position) t))
-        (setq beg-marker (point-marker)))
-      (save-excursion
-        (skip-chars-forward " \t\n\r")
-        (setq beg-pos (point))
-        (goto-char end-marker)
-        (skip-chars-backward " \t\n\r")
-        (setq end-pos (point)))
-      (when (or
-             (< end-pos beg-pos)
-             (and (= (line-number-at-pos beg-pos) (line-number-at-pos end-pos))
-                  (< (+ (current-column) (- end-pos beg-pos) 2) fill-column)))
-        (just-one-space -1)
-        (goto-char end-marker)
-        (just-one-space -1))
-      (goto-char beg-marker)
-      t)))
