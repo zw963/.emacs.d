7b785bad43f47962879b9747f65dcb8798994eb4
Billy.Zheng <vil963@gmail.com>
2026-01-23 19:12:06 +0800

    disable liger for lsp

--- plugins/crystal-mode/lsp-crystal_init.el
+++ plugins/crystal-mode/lsp-crystal_init.el
@@ -1,7 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 
 ;; 注意，这里不要首先 require lsp-crystal, 否则将用默认的 scry 作为 server.
-(setq lsp-clients-crystal-executable '("liger" "--stdio"))
+;; (setq lsp-clients-crystal-executable '("liger"))
 (require 'lsp-crystal)
 (require 'lsp-mode_init)
 
